56419315d684747157156f41ed3fbc98aba3c8b2
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2021-11-02 21:41:21 +0200

    Initial commit

---
+++ backend/configs/configs.js
@@ -0,0 +1,12 @@
+const DB_USER = process.env.MONGO_INITDB_ROOT_USERNAME
+const DB_PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD
+const DB_NAME = process.env.MONGO_INITDB_DATABASE
+const DB_HOST = process.env.DB_HOST
+const DB_PORT = process.env.DB_PORT
+
+
+module.exports = {
+    PORT: process.env.PORT,
+    HOST: process.env.HOST,
+    MONGO_URI: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+}
